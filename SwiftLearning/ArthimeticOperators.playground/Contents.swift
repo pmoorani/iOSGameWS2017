338fed58b4a88c5e8c05842f6c03398513e82ee7
MuhammadAhmad902 <ahmadabbas902@gmail.com>
2018-01-30 03:06:14 +0100

    dictionary in swift

--- SwiftLearning/ArthimeticOperators.playground/Contents.swift
+++ SwiftLearning/ArthimeticOperators.playground/Contents.swift
@@ -13,4 +13,14 @@ num1 - num2
 num1 * num2
 num1 / num2
 
+num1 % num2
+
+num1 += 1
+num2 -= 2
+
+num1 += num2
+//escape char '\' use to enter qoutation marks in string
+
+var sent = "my Name is \"AHMAD\""
+print(sent)s
 
